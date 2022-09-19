3766f75ef35c7e7960bce772cd08db87164b4f30
gaurav_kotecha <gauravkotecha1@gmail.com>
2022-09-19 16:49:18 +0530

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hey there, I am Learning CD Pipeline using AWS'));
+app.get('/', (req, res) => res.send('Hey there I am adding Manual approval action in the pipeline execution to stop so someone can manually approve or reject the action.  '));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
